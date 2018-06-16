98a8c6c8eb45b225f67c9071da95fbcaa6608626
Aurora Vaughn <avaughn.git@gmail.com>
2018-06-16 12:34:27 -0500

    Setting up file structure and console greeting
    I created my js folder and index.js file. I wrote a quick function to log out a greeting so if someone looks in the console they are encouraged to reach out to me. 
    
    This will also confirm my script tag is getting my index.js file.

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+function hello(){
+console.log(`Hello! Thank you for the taking an interest in my code!
+There's not a lot going on here at the moment.
+To talk about some of the more exciting things I have done e-mail me at:
+vaughn.aurora@gmail.com
+
+or add me on linkedin
+
+linkedin.com/in/aurora-vaughn
+
+See you soon! 
+`)
+}
+
+hello()
